fa80b7317fcf0b41b4183c3cc4c6cc70a0bbc586
Georgie <georginambaker@outlook.com>
2023-11-26 20:52:40 +0000

    git remote add origin https://github.com/Gbaker97/snake_game.git   git branch -M main   git push -u origin main

---
+++ main.py
@@ -0,0 +1,58 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+def choose_mode():
+    mode_choice = screen.textinput("Select a mode.", "Type 'easy', 'medium', or 'hard': ").lower()
+    if mode_choice == "hard":
+        snake.game_mode = "fastest"
+    elif mode_choice == "medium":
+        snake.game_mode = "fast"
+    for s in snake.snake_body:
+        s.speed = snake.game_mode
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+scoreboard = Scoreboard()
+snake = Snake()
+food = Food()
+choose_mode()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.update()
+        scoreboard.add_point()
+        snake.extend()
+
+    # detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_on = False
+        scoreboard.game_over()
+
+    # detect collision with tail
+    for seg in snake.snake_body[1:]:
+        if snake.head.distance(seg) < 10:
+            game_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("JetBrains Mono", 14, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.shape("blank")
+        self.penup()
+        self.goto(0, 280)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
+
+    def add_point(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", False, ALIGNMENT, FONT)
